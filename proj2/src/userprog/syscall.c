aa497bf9c1174ca3058dcc01244c9b1cb6b30520
Caleb Simmeth <simmeth@usc.edu>
2016-02-25 14:48:42 -0800

    add command line prints

--- proj2/src/userprog/syscall.c
+++ proj2/src/userprog/syscall.c
@@ -5,13 +5,35 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "devices/shutdown.h"
+#include "userprog/process.h"
 
 static void syscall_handler (struct intr_frame *);
+
+static void exit(struct intr_frame *f);
+static void write(struct intr_frame *f);
+static void halt(void);
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void * 
+get_arg(struct intr_frame *f, int arg){
+
+  return f->esp + (arg * sizeof(int));
+}
+
+static int
+get_int(struct intr_frame *f, int arg){
+  return *(int*)get_arg(f,arg);
+}
+
+static char*
+get_char_ptr(struct intr_frame *f, int arg){
+  return *(char**)get_arg(f,arg);
 }
 
 static void
@@ -31,13 +53,58 @@ syscall_handler (struct intr_frame *f)
      thread_exit ();
   }
 
-  int syscall = *(int*)f->esp;
-  printf ("Call Number: %d\n",syscall);
+  int syscall = get_int(f,0); 
+
+  printf("Syscall: %d\n",syscall);
+  
 
   switch(syscall){
-   // fill this out	
+
+	case SYS_HALT:
+	    halt();
+		break;
+
+	case SYS_EXIT:
+	    exit(f);
+		break;
+
+	case SYS_WRITE:
+	   write(f);
+	   break;
+
+	default:
+       printf ("system call unknown!\n");
+       thread_exit ();
+
   }
 
-  printf ("system call unknown!\n");
-  thread_exit ();
+
 }
+
+static void
+halt(void)
+{
+  shutdown_power_off();
+}
+
+static void 
+exit(struct intr_frame * f)
+{
+  int status = get_int(f,1);
+  printf("Status: %d\n",status);
+  f->eax = status;
+  process_exit();
+}
+
+static void 
+write(struct intr_frame * f)
+{
+  int fd = get_int(f,1);
+  char * buffer = get_char_ptr(f,2);
+  int size = get_int(f,3);
+  putbuf(buffer,size);
+
+}
+
+//TODO synch for all file sys calls
+//deny writes to the running file
