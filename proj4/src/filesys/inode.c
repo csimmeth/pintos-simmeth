a15d14184af663be057ae8dd441f5b62d9b5c11b
Caleb Simmeth <simmeth@usc.edu>
2016-04-22 02:35:38 -0700

    inode not working

--- proj4/src/filesys/inode.c
+++ proj4/src/filesys/inode.c
@@ -68,12 +68,9 @@ byte_to_sector (const struct inode *inode, off_t pos)
 static void
 allocate_block(block_sector_t * psector, block_sector_t block, int ofs)
 {
-  // Check if there is not a sector allocated yet
 	// Get a new sector
 	
     free_map_allocate (1, psector);
-	///printf("result: %i\n",b);
-	//printf("found section free: %i\n",*psector);
 
 	// Zero out the new sector
     static char zeros[BLOCK_SECTOR_SIZE];
@@ -87,66 +84,67 @@ static block_sector_t
 byte_to_psector(struct inode *inode, off_t pos,int size)
 {
   
+  
   bool grow = false;
-  if(bytes_to_sectors(pos+size) > inode->data_sectors)
+  
+  /*
+  if(bytes_to_sectors(pos) > inode->data_sectors)
   {
-	uint32_t new_sectors = bytes_to_sectors(pos+size);
+	uint32_t new_sectors = bytes_to_sectors(pos);
     grow = true;
 	//printf("data_sectors: %d, old: %d \n",new_sectors,inode->data_sectors);
-
 	inode->data_sectors = new_sectors; 
 	cache_write(inode->sector,&inode->data_sectors,4,4);
   }
+  */
  
   block_sector_t psector;
-
   block_sector_t vsector = pos / BLOCK_SECTOR_SIZE;
-  //printf("Thread %s \n",thread_name());
-  //printf("trying to find vsector %d\n",vsector);
-  if(vsector < 12)
+
+  if(vsector < 126)
   {
 	// Calculate the offset
 	int ofs = vsector * 4 + INODE_OFS;
 
-	// Read sector directly from inode_disk
-	 
+	// Read sector or allocate directly from inode_disk
 	if(grow)
 	{
 	  allocate_block(&psector,inode->sector,ofs);
-	  int temp; 
-	  cache_read(inode->sector,&temp,ofs,4);
-	  //printf("reading: %d\n",temp);
-	  //printf("allocated block %d for inode %d at position %d\n",psector,inode->sector,ofs);
 	}
 	else
 	{
       cache_read(inode->sector,&psector,ofs,4);
-	  //printf("read sector %d from inode %d at position %d\n",psector,inode->sector,ofs);
 	}
 
-	//  if(psector == 0)
-	///	allocate_block(&psector,inode->sector,ofs);
-
-	//printf("got psector %d from vsector %d\n",psector,vsector);
-
-	//allocate_if_null(&psector,inode->sector,ofs);
+//	printf("got psector %d from vsector %d\n",psector,vsector);
 
 	return psector;
   }
   else if(vsector < 140)
   {
+	printf("here\n");
 	int ofs = 48 + INODE_OFS;
 
 	// Get the intermediate block
     block_sector_t iblock; 
 	cache_read(inode->sector,&iblock,ofs,4);
+	if(iblock == 0)
+	{
+	  allocate_block(&iblock,inode->sector,ofs);
+	}
 
-	//allocate_block(&iblock,inode->sector,ofs);
-
-	// Read the sector from the iblock
 	vsector = vsector - 12;
 	ofs = 4 * vsector;
-	cache_read(iblock,&psector,ofs,4);
+
+	if(grow)
+	{
+	  allocate_block(&psector,iblock,ofs);
+	}
+	else
+	{
+	  // Read the sector from the iblock
+	  cache_read(iblock,&psector,ofs,4);
+	}
 
 	//allocate_block(&psector,iblock,ofs);
 	return psector;
@@ -157,8 +155,9 @@ byte_to_psector(struct inode *inode, off_t pos,int size)
 	int ofs = 52 + INODE_OFS;
 	block_sector_t double_iblock;
 	cache_read(inode->sector,&double_iblock,ofs,4);
+	//printf("double at block %d\n",double_iblock);
 
-    allocate_block(&double_iblock,inode->sector,ofs);
+//    allocate_block(&double_iblock,inode->sector,ofs);
 
 	// Calculate where to look in the double block
 	vsector = vsector - 140;
@@ -167,14 +166,17 @@ byte_to_psector(struct inode *inode, off_t pos,int size)
 
 	// Get the second iblock
 	cache_read(double_iblock,&iblock,block_ofs,4);
+	//printf("iblock at %d\n",iblock);
 
-	allocate_block(&iblock,double_iblock,block_ofs);
+	//allocate_block(&iblock,double_iblock,block_ofs);
 
 	// Check where to look in the second block
 	int sector_ofs = (vsector % 128 ) * 4;
 	cache_read(iblock,&psector,sector_ofs,4);
 
-	allocate_block(&psector,iblock,sector_ofs);
+	//printf("sector at %d\n",psector);
+
+	//allocate_block(&psector,iblock,sector_ofs);
     return psector;
   }
   else 
@@ -216,7 +218,7 @@ inode_create (block_sector_t sector, off_t length)
 
       size_t sectors = bytes_to_sectors (length);
       disk_inode->length = length; //12 * BLOCK_SECTOR_SIZE;
-	  disk_inode->sec_alloc =0;// sectors;
+	  disk_inode->sec_alloc = 0;// sectors;
       disk_inode->magic = INODE_MAGIC;
 
       //cache_create(sector,disk_inode);
@@ -240,15 +242,17 @@ inode_create (block_sector_t sector, off_t length)
               static char zeros[BLOCK_SECTOR_SIZE];
               size_t i;
               
+				//free_map_allocate(sectors,&disk_inode->sectors[i]);
 				//printf("allocating %i sectors\n",sectors);
               for (i = 0; i < sectors; i++) 
 			  {
-				if(i < 12)
+				if(i < 126)
 				{
-				free_map_allocate(1,&disk_inode->sectors[i]);
 				//disk_inode->sectors[i] = disk_inode->sectors[0] + i;
                 //block_write (fs_device, disk_inode->start + i, zeros);
 
+				  free_map_allocate(1,&disk_inode->sectors[i]);
+				//disk_inode->sectors[i] = disk_inode->sectors[0] + i;
 				cache_create(disk_inode->sectors[i], zeros);
 				}
 				else if(i < 140)
@@ -260,13 +264,13 @@ inode_create (block_sector_t sector, off_t length)
 					free_map_allocate(1,&disk_inode->sectors[i]);
 				    cache_create(disk_inode->sectors[i], zeros);
 				  }
-	              int ofs = 48 + INODE_OFS;
+
 				  block_sector_t iblock = disk_inode->sectors[12];
 
 				  // Read the sector from the iblock
 				  block_sector_t vsector = i;
 				  vsector = vsector - 12;
-				  ofs = 4 * vsector;
+				  int ofs = 4 * vsector;
 				  block_sector_t new_block;
 				  //Allocate a new block
 				  free_map_allocate(1,&new_block);
@@ -275,6 +279,33 @@ inode_create (block_sector_t sector, off_t length)
 				  //Write this to the indirect node
 				  cache_write(iblock,&new_block,ofs,4);
 
+				}
+
+                else if(i < 16524)
+				{
+                  if(i == 140)
+				  {
+					free_map_allocate(1,&disk_inode->sectors[13]);
+					cache_create(disk_inode->sectors[13],zeros);
+				  }	
+                  block_sector_t double_iblock = disk_inode->sectors[13];
+
+				  block_sector_t vsector = i;
+				  vsector = vsector -140;
+				  int block_ofs = (vsector / 128) * 4;
+				  int sector_ofs = (vsector % 128) * 4;
+				  block_sector_t iblock;
+				  if(sector_ofs == 0)
+				  {
+				    free_map_allocate(1,&iblock);
+					cache_create(iblock,zeros);
+					cache_write(double_iblock,&iblock,block_ofs,4);	
+				  }
+
+				  block_sector_t new_block;
+				  free_map_allocate(1,&new_block);
+				  cache_create(new_block,zeros);
+				  cache_write(iblock,&new_block,sector_ofs,4);
 				}
 			  }
 			  disk_inode->sec_alloc =sectors;
@@ -406,6 +437,7 @@ inode_read_at (struct inode *inode, void *buffer_, off_t size, off_t offset)
       if (chunk_size <= 0)
         break;
 
+	  //printf("reading from sector %d\n",sector_idx);
 	  cache_read(sector_idx,buffer+bytes_read, sector_ofs, chunk_size);
 
       /* Advance. */
@@ -452,6 +484,7 @@ inode_write_at (struct inode *inode, const void *buffer_, off_t size,
       if (chunk_size <= 0)
         break;
 
+	  //printf("writing to sector %d\n",sector_idx);
       cache_write(sector_idx, buffer + bytes_written,
 			 		  sector_ofs,chunk_size);
 
